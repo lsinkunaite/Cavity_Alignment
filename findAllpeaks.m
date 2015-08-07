f775b8881330a8a3f4912f7970f922adadee4469
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-07 12:20:02 -0700

    Plots the input matrix and marks the peaks on the plot

--- findAllpeaks.m
+++ findAllpeaks.m
@@ -2,4 +2,9 @@ function [pks,locs] = findAllpeaks(input_matrix)
 % Finds peaks and their positions including the endpoints
     pks=find(input_matrix>=[input_matrix(2:end) -inf] & input_matrix>[-inf input_matrix(1:end-1)]);
     locs=input_matrix(pks);
+    figure;
+    plot(input_matrix);
+    hold on;
+    plot(pks, locs,'or');
+    xlim([0 (length(input_matrix)-1)]);
 end
