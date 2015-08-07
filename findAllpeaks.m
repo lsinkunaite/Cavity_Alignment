dc40b6ae133bdeaf40c362ffa6e09c0ce0ce5509
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-07 14:42:48 -0700

    Fixed bug: swapped locs and pks arrays

--- findAllpeaks.m
+++ findAllpeaks.m
@@ -1,10 +1,10 @@
 function [pks,locs] = findAllpeaks(input_matrix)
 % Finds peaks and their positions including the endpoints
-    pks=find(input_matrix>=[input_matrix(2:end) -inf] & input_matrix>[-inf input_matrix(1:end-1)]);
-    locs=input_matrix(pks);
+    locs=find(input_matrix>=[input_matrix(2:end) -inf] & input_matrix>[-inf input_matrix(1:end-1)]);
+    pks=input_matrix(locs);
     figure;
     plot(input_matrix);
     hold on;
-    plot(pks, locs,'or');
+    plot(locs, pks,'or');
     xlim([0 (length(input_matrix)-1)]);
 end
