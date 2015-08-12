1e8c1dbbabdee4c74b995264171c6a6d4624fd48
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-12 10:49:31 -0700

    Added "neighbourhood condition"

--- Ratio_Table.m
+++ Ratio_Table.m
@@ -1,5 +1,6 @@
-function [Ratio_Matrix] = Ratio_Table(fitting_path,rTABLE_filename,results_filename2,Table_Matrix,bash_filename2)
-% Prints relative ratios of power spectrum in a tabular form
+function [Ratio_Matrix] = Ratio_Table(pkr,fitting_path,rTABLE_filename,results_filename2,Table_Matrix,bash_filename2)
+% Prints relative power ratios in a tabular form.
+% v2.0: included neigbourhood condition: checks if it is a close neighbour.
     rTABLE=fopen(strcat(fitting_path,rTABLE_filename,results_filename2),'at');
     for Table_Iter_Row=1:size(Table_Matrix,1)
         if (size(Table_Matrix,2))>2
@@ -8,8 +9,11 @@ function [Ratio_Matrix] = Ratio_Table(fitting_path,rTABLE_filename,results_filen
                 row_el=Table_Matrix(Table_Iter_Row,Table_Iter);
                 for Table_Iter2=(Table_Iter+1):size(Table_Matrix,2)
                     row_el2=Table_Matrix(Table_Iter_Row,Table_Iter2);
-                    T_Index_Iter=T_Index_Iter+1;
-                    rTable_Matrix(T_Index_Iter)=row_el2/row_el;
+                    % Neighbourhood condition
+                    if ((row_el2-row_el)<=(length(pkr)))
+                        T_Index_Iter=T_Index_Iter+1;
+                        rTable_Matrix(T_Index_Iter)=row_el2/row_el;
+                    end
                 end
             end
         end
