01068b33b05c4f72d5a02de88bc39aa0066bf15d
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-13 16:17:31 -0700

    Calculates the misalignment parameter to the right of the highest peak

--- Misalignment.m
+++ Misalignment.m
@@ -1,14 +1,20 @@
-function [Misalignment_Array]=Misalignment(tol_Index,Ratio_Matrix,pkr)
-% (Sometimes) Finds misalignment parameter comparing the ratios of
-% intensities
-    Misalignment_Array=[];
-    for Ratio_Iter=1:(size(Ratio_Matrix,1))
-        [tfs,locations]=ismembertol(pkr,Ratio_Matrix(Ratio_Iter,:),tol_Index);
-        %if ((length(unique(locations))) == (length(locations)))
-        if (length(locations([true;diff(locations(:))>0])) == length(locations))
-            if all(tfs);
-                %Misalignment=[Misalignment Ratio_Matrix(Ratio_Iter,1)];
-                Misalignment_Array=[Misalignment_Array Ratio_Iter];
+function [Min_Dist,Mis_Row,Mis_Mode] = Misalignment(Ratio_Matrix,sorted_pkr)
+% Returns misalignment parameter
+    Mis_Row=0;
+    Mis_Mode=0;
+    Min_Dist=inf;
+    for RRow_Iter=1:(size(Ratio_Matrix,1))
+        for RColumn_Iter=1:(size(Ratio_Matrix,2)-length(sorted_pkr))
+            Dist=0;
+            for pkr_Iter2=1:length(sorted_pkr)
+                Dist=Dist+power(sorted_pkr(pkr_Iter2)-Ratio_Matrix(RRow_Iter,RColumn_Iter+pkr_Iter2),2);
+            end
+            Inter_Dist=power(Dist,.5);
+            %fprintf('Row = %d, Mode = %d, Inter_Dist = %f, Min_Dist = %f\n',RRow_Iter,RColumn_Iter-1,Inter_Dist,Min_Dist);
+            if (Inter_Dist<Min_Dist)
+                Min_Dist=Inter_Dist;
+                Mis_Row=RRow_Iter;
+                Mis_Mode=RColumn_Iter-1;
             end
         end
     end
