55393039b91fb5a763b68154816983eab4064ee0
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-15 17:15:53 -0700

    Fixed the double inclusion of the same peak at the end points

--- findAllpeaks.m
+++ findAllpeaks.m
@@ -1,6 +1,10 @@
 function [pks,locs] = findAllpeaks(input_matrix)
 % Finds peaks and their positions including the endpoints
-    locs=find(input_matrix>=[input_matrix(2:end) -inf] & input_matrix>[-inf input_matrix(1:end-1)]);
+    %locs=find(input_matrix>=[input_matrix(2:end) -inf] & input_matrix>[-inf input_matrix(1:end-1)]);
+    locs=find(input_matrix>=[input_matrix(2:end) inf] & input_matrix>[-inf input_matrix(1:end-1)]);
+    if (input_matrix(1)<input_matrix(end-1))
+        locs=locs(2:end);
+    end
     pks=input_matrix(locs);
     figure;
     plot(input_matrix);
