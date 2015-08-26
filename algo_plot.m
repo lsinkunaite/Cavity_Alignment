e6719f5d353ed973f1c62ab1d34363a65134bb76
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-26 12:10:28 -0700

    Updated plots for algorithm testing

--- algo_plot.m
+++ algo_plot.m
@@ -1,41 +1,66 @@
 close all;
 clear all;
 fitting_path='/home/laurasinkunaite/Finesse2.0/Misalignment/Fitting/';
-file_1D='1D_test.txt';file_3D='2D_test.txt';file_2D='3D_test.txt';
-Matrix_1D=csvread(strcat(fitting_path,file_1D));
-[sorted_1D,locs_1D]=sort(Matrix_1D(:,1));
-tmp_1D=Matrix_1D(:,2); tmp_1D=tmp_1D(locs_1D);
-Matrix_2D=csvread(strcat(fitting_path,file_2D));
-[sorted_2D,locs_2D]=sort(Matrix_2D(:,1));
-tmp_2D=Matrix_2D(:,2); tmp_2D=tmp_2D(locs_2D);
-Matrix_3D=csvread(strcat(fitting_path,file_3D));
-[sorted_3D,locs_3D]=sort(Matrix_3D(:,1));
-tmp_3D=Matrix_3D(:,2); tmp_3D=tmp_3D(locs_3D);
+file_1D_10='1D_test_maxTEM10.txt';file_1D_15='1D_test_maxTEM15.txt';file_1D_20='1D_test_maxTEM20.txt';
+file_2D_10='2D_test_maxTEM10.txt';file_2D_15='2D_test_maxTEM15.txt';file_2D_20='2D_test_maxTEM20.txt';
+file_3D_10='3D_test_maxTEM10.txt';file_3D_15='3D_test_maxTEM15.txt';file_3D_20='3D_test_maxTEM20.txt';
+
+Matrix_1D_10=csvread(strcat(fitting_path,file_1D_10));
+[sorted_1D_10,locs_1D_10]=sort(Matrix_1D_10(:,1));
+tmp_1D_10=Matrix_1D_10(:,2); tmp_1D_10=tmp_1D_10(locs_1D_10);
+Matrix_1D_15=csvread(strcat(fitting_path,file_1D_15));
+[sorted_1D_15,locs_1D_15]=sort(Matrix_1D_15(:,1));
+tmp_1D_15=Matrix_1D_15(:,2); tmp_1D_15=tmp_1D_15(locs_1D_15);
+Matrix_1D_20=csvread(strcat(fitting_path,file_1D_20));
+[sorted_1D_20,locs_1D_20]=sort(Matrix_1D_20(:,1));
+tmp_1D_20=Matrix_1D_20(:,2); tmp_1D_20=tmp_1D_20(locs_1D_20);
+
+Matrix_2D_10=csvread(strcat(fitting_path,file_2D_10));
+[sorted_2D_10,locs_2D_10]=sort(Matrix_2D_10(:,1));
+tmp_2D_10=Matrix_2D_10(:,2); tmp_2D_10=tmp_2D_10(locs_2D_10);
+Matrix_2D_15=csvread(strcat(fitting_path,file_2D_15));
+[sorted_2D_15,locs_2D_15]=sort(Matrix_2D_15(:,1));
+tmp_2D_15=Matrix_2D_15(:,2); tmp_2D_15=tmp_2D_15(locs_2D_15);
+Matrix_2D_20=csvread(strcat(fitting_path,file_2D_20));
+[sorted_2D_20,locs_2D_20]=sort(Matrix_2D_20(:,1));
+tmp_2D_20=Matrix_2D_20(:,2); tmp_2D_20=tmp_2D_20(locs_2D_20);
+
+Matrix_3D_10=csvread(strcat(fitting_path,file_3D_10));
+[sorted_3D_10,locs_3D_10]=sort(Matrix_3D_10(:,1));
+tmp_3D_10=Matrix_3D_10(:,2); tmp_3D_10=tmp_3D_10(locs_3D_10);
+Matrix_3D_15=csvread(strcat(fitting_path,file_3D_15));
+[sorted_3D_15,locs_3D_15]=sort(Matrix_3D_15(:,1));
+tmp_3D_15=Matrix_3D_15(:,2); tmp_3D_15=tmp_3D_15(locs_3D_15);
+Matrix_3D_20=csvread(strcat(fitting_path,file_3D_20));
+[sorted_3D_20,locs_3D_20]=sort(Matrix_3D_20(:,1));
+tmp_3D_20=Matrix_3D_20(:,2); tmp_3D_20=tmp_3D_20(locs_3D_20);
+
+
 figure(1)
-plot(sorted_1D(:,1),tmp_1D,'r');
+plot(sorted_3D_10(:,1),tmp_3D_10,'r');
 hold on;
-plot(sorted_2D(:,1),tmp_2D,'b');
+plot(sorted_3D_15(:,1),tmp_3D_15,'b');
 hold on;
-plot(sorted_3D(:,1),tmp_3D,'k');
+plot(sorted_3D_20(:,1),tmp_3D_20,'k');
 hold on;
 x2=0:0.01:5; y2=x2;
 plot(x2,y2,'-.k');
-legend('1D','2D','3D','Location','northwest');
+legend('maxTEM=10','maxTEM=15','maxTEM=20','Location','northwest');
 xlim([0 5]);
 xlabel('Calculated misalignment');
 ylabel('Gouy misalignment');
-title({'Misalignment algorithm applied to', 'Finesse simulation (maxTEM=20)'});
+title({'Misalignment algorithm applied to', 'Finesse simulation (3D case)'});
 figure(2)
-plot(sorted_1D(:,1),tmp_1D-sorted_1D(:,1),'r');
+plot(sorted_3D_10(:,1),tmp_3D_10-sorted_3D_10(:,1),'r');
 hold on;
-plot(sorted_2D(:,1),tmp_2D-sorted_2D(:,1),'b');
+plot(sorted_3D_15(:,1),tmp_3D_15-sorted_3D_15(:,1),'b');
 hold on;
-plot(sorted_3D(:,1),tmp_3D-sorted_3D(:,1),'k');
+plot(sorted_3D_20(:,1),tmp_3D_20-sorted_3D_20(:,1),'k');
 hold on;
 x2=0:0.01:5; y2=zeros(length(x2));
 plot(x2,y2,'-.k');
-legend('1D','2D','3D','Location','southwest');
+legend('maxTEM=10','maxTEM=15','maxTEM=20','Location','southwest');
 xlim([0 5]);
 xlabel('Calculated misalignment');
 ylabel('(Gouy-calculated) misalignment');
-title({'Misalignment algorithm applied to', 'Finesse simulation (maxTEM=20)'});
+title({'Misalignment algorithm applied to', 'Finesse simulation (3D case)'});
