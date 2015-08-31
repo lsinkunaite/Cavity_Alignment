46712aaac723c0378f071580af4256fc3e09f310
lsinkunaite <sinkunaite.lp@gmail.com>
2015-08-31 10:55:30 -0700

    Finds peaks using difference method, works for row vectors but can be easily extended to matrices

--- findTruepeaks.m
+++ findTruepeaks.m
@@ -1,21 +1,17 @@
 function [pks,locs] = findTruepeaks(input_matrix)
 % Finds peaks and their positions including the endpoints (excluding
 % neighbouring peaks)
-    %locs=find(input_matrix>=[input_matrix(2:end) -inf] & input_matrix>[-inf input_matrix(1:end-1)]);
-    locs=find(input_matrix>=[input_matrix(2:end) inf] & input_matrix>[-inf input_matrix(1:end-1)]);
-    if (input_matrix(1)<input_matrix(end-1))
-        locs=locs(2:end);
-    end
+    %locs=find(input_matrix>=[input_matrix(2:end) inf] & input_matrix>[-inf input_matrix(1:end-1)] & input_matrix>3);
+    input_matrix2=zeros(1,length(input_matrix)+2);
+    input_matrix2(1,2:end-1)=input_matrix;
+    Diff_Left=input_matrix2(1,2:end-1)-input_matrix2(1,1:end-2);
+    Diff_Right=input_matrix2(1,2:end-1)-input_matrix2(1,3:end);
+    locs=find(Diff_Left > 0 & Diff_Right > 0);
     pks=input_matrix(locs);
-    
-    % Locations where slope changes its sign
-    new_locs=(find((diff(sign(diff(input_matrix)))~=0)~=0))+1;
-    new_pks=input_matrix(new_locs);
-    
-    
-    figure;
-    plot(input_matrix);
-    hold on;
-    plot(locs, pks,'or');
-    xlim([0 (length(input_matrix)-1)]);
+%     
+%     %figure;
+%     plot(input_matrix);
+%     hold on;
+%     plot(locs, pks,'or');
+%     xlim([0 (length(input_matrix)-1)]);
 end
