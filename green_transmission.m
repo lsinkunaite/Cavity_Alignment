b95865e9df37ff16d3ca2f9a3776a8abe5016469
lsinkunaite <sinkunaite.lp@gmail.com>
2015-09-01 17:45:13 -0700

    Current version of main

--- green_transmission.m
+++ green_transmission.m
@@ -16,7 +16,7 @@ alpha0 = (power((lambda/pi),.5))*(power((tL*(R_etm-tL)),(-.25)));
 z_R=(pi*(power(w0,2)))/lambda; % Rayleigh range
 
 % Iterates over measured data (i = 7)
-for i=2:2
+for i=7:7
     Input_Matrix_16k=csvread(strcat(data_path,sprintf(data_name_16k,num2str(i))));
     Input_Matrix_256=csvread(strcat(data_path,sprintf(data_name_256,num2str(i))));
 
@@ -26,7 +26,8 @@ for i=2:2
     % Ratio between domains
     Domain_factor=(length(Input_Matrix_16k(:,2)))/(length(Input_Matrix_256(:,2)));
 
-
+    antires=5;antispike=.02;
+    
     % Plots power vs relative displacement
     figure();
     x=1:1:(length(Input_Matrix_256(:,2))); 
@@ -63,15 +64,14 @@ for i=2:2
     [pks2,locs2]=findAllpeaks(Input_Matrix_16k(Domain_From:Domain_To,2)');
     title('findAllpeaks');
     subplot(3,1,3);
-    [pks3,locs3]=findTruepeaks(Input_Matrix_16k(Domain_From:Domain_To,2)');
+    [pks3,locs3]=findTruepeaks(Input_Matrix_16k(Domain_From:Domain_To,2)',antires,antispike);
     title('findTruepeaks');
     
     mode_algo=5;
     [pks_new,locs_new]=findpeaks(Input_Matrix_16k(:,2)');
     [extreme_pks,extreme_locs]=findExtremePeaks(pks,locs,.5);
     [Domain_From_new,Domain_To_new]=Extreme(Input_Matrix_16k(:,2),pks_new,locs_new,0.5,0.2);
-    %[Domain_From_new,Domain_To_new]=Extreme(Input_Matrix_16k(:,2),pks_new,locs_new,.5,.2);
-    [pks_local,locs_local]=findpeaks(Input_Matrix_16k(Domain_From_new:Domain_To_new,2)');
+    [pks_local,locs_local]=findTruepeaks(Input_Matrix_16k(Domain_From_new:Domain_To_new,2)',antires,antispike);
     if ((length(pks_local))>mode_algo)
         [pks_algo,locs_algo]=sort(pks_local,'descend');
         locs_algo=locs_local(locs_algo);
@@ -90,9 +90,15 @@ for i=2:2
 
     % Returns misalignment parameter in a given range
     gouy_shift=(atan(tL/z_R)-atan(-(L-tL)/z_R))*180/pi; % Gouy phase shift
-    [gouy_peaks,gouy_locs] = Gouy_Sort(pkr(:),locs(:),gouy_shift);
-    [Dist_Gouy,Row_Gouy,Mode_Gouy]=Misalignment(Ratio_Matrix,gouy_peaks);
+    [gouy_pks,gouy_locs] = Gouy_Sort(pkr(:),locs(:),gouy_shift);
+    [Dist_Gouy,Row_Gouy,Mode_Gouy,Tgouy_pks]=TrueMisalignment(Ratio_Matrix,gouy_pks);
     Mis_Par_Gouy=Ratio_Matrix(Row_Gouy,1)
+    Ratio_Matrix(Row_Gouy,:)
+    Tgouy_pks
+    [c, index]= min(abs(Ratio_Matrix(:,1)-2.3516));
+    Ratio_Matrix(index,:)
+    Mode_Gouy
+    Mis_Par_Gouy
 
     figure();
     plot(Interpolated_y(Domain_From:Domain_To),Input_Matrix_16k(Domain_From:Domain_To,2));
