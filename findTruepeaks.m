8f8d4396d2ce8c030bb01b63ef5c5f9db05dc373
lsinkunaite <sinkunaite.lp@gmail.com>
2015-09-03 15:35:18 -0700

    Updated peak finding definition

--- findTruepeaks.m
+++ findTruepeaks.m
@@ -6,9 +6,31 @@ function [pks,locs] = findTruepeaks(input_matrix,antires,antispike)
     Diff_Left=input_matrix2(1,2:end-1)-input_matrix2(1,1:end-2);
     Diff_Right=input_matrix2(1,2:end-1)-input_matrix2(1,3:end);
     locs=find(Diff_Left > 0 & Diff_Right > 0);
+    valley_locs=find(Diff_Left > 0 & Diff_Right > 0);
+    
+    figure;
+    subplot(4,1,1);
+    plot(input_matrix);
+    hold on;
+    plot(locs,input_matrix(locs),'vr');
+    title('peaks');
+    
     locs((find(((diff(locs))<antires)&((sign(diff(input_matrix(locs))))<1)))+1)=[];
+    
+    subplot(4,1,2);
+    plot(input_matrix);
+    hold on;
+    plot(locs,input_matrix(locs),'vr');
+    title('antires L2R');
+    
     locs((find(((diff(locs))<antires)&((sign(diff(fliplr(input_matrix(locs)))))<1)))+1)=[];
     pks=input_matrix(locs);
+    
+    subplot(4,1,3);
+    plot(input_matrix);
+    hold on;
+    plot(locs,input_matrix(locs),'vr');
+    title('antires R2L');
     
     Slope_Array=[0 (diff(sign(diff(input_matrix)))==0) 0];
     prom(1)=pks(1)-(input_matrix(find((Slope_Array(locs(1)+1:end))==0,1)+locs(1)-1));
@@ -26,10 +48,40 @@ function [pks,locs] = findTruepeaks(input_matrix,antires,antispike)
             prom(pk_iter)=pks(pk_iter);
         end
     end
+
     prom_ratio=prom/(max(prom));
-    locs(find(prom_ratio<antispike))=[];
+    InterSpace=length(input_matrix)/(length(pks));
+    locs((find((prom_ratio(1:end-1)<antispike)&((sign(diff(pks)))<1)&((diff(locs))<InterSpace)))+1)=[];   
     pks=input_matrix(locs);
- 
+    
+    prom=[];
+    prom(1)=pks(1)-(input_matrix(find((Slope_Array(locs(1)+1:end))==0,1)+locs(1)-1));
+    prom(length(pks))=input_matrix(find((Slope_Array(1:locs(length(pks))-1))==0,1,'last'));
+    for pk_iter=2:(length(pks)-1)
+        pk_R=input_matrix(find((Slope_Array(locs(pk_iter)+1:end))==0,1)+locs(pk_iter)-1);
+        pk_L=input_matrix(find((Slope_Array(1:locs(pk_iter)-1))==0,1,'last'));
+        if (((length(pk_L))~=0)&&((length(pk_R))~=0))
+            if ((pks(pk_iter)-pk_R)<(pks(pk_iter)-pk_L))
+                prom(pk_iter)=pks(pk_iter)-pk_R;
+            else
+                prom(pk_iter)=pks(pk_iter)-pk_L;
+            end
+        else
+            prom(pk_iter)=pks(pk_iter);
+        end
+    end
+    
+    prom_ratio=prom/(max(prom));
+    InterSpace=length(input_matrix)/(length(pks));
+    locs((find((fliplr(prom_ratio(2:end))<antispike)&((sign(diff(pks)))<1)&((diff(locs))<InterSpace)))+1)=[];    
+    pks=input_matrix(locs);
+    
+    subplot(4,1,4);
+    plot(input_matrix);
+    hold on;
+    plot(locs,input_matrix(locs),'vr');
+    title('antispike');
+    
     figure;
     plot(input_matrix);
     hold on;
